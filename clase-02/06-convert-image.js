e4d673f03a41cf6a3644efb2aac07582557e6c9c
jjco.dev <mail@jjco.dev>
2025-02-05 04:57:13 -0600

    clase 02

--- clase-02/06-convert-image.js
+++ clase-02/06-convert-image.js
@@ -1,10 +1,10 @@
 const {spawn} = require('node:child_process');
 const fs = require('fs');
 
-let img = 'image1.jpg';
+let img = 'imagen1';
 
 // cwebp -q 80 image.png -o image.webp
-const convertImg = spawn('cwebp', ['-q', '75', img, '-o', `imagenX.webp`] );
+const convertImg = spawn('cwebp', ['-q', '75', `${img}.jpg`, '-o', `${img}.webp`] );
 
 convertImg.stdout.on('data', data => {
 	console.log(`stdout: ${data}`);
